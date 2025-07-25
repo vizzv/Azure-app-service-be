41618e8a2bfe5b927e374f4caf2d2782126aa668
vishw patel <66671671+vizzv@users.noreply.github.com>
2025-07-25 11:47:53 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -43,22 +43,6 @@ var app = builder.Build();
     // global cors policy
 app.UseCors("AllowFrontendApp");
 
-app.Use(async (context, next) =>
-{
-    var tokenFromHeader = context.Request.Headers["X-Frontend-Secret"].FirstOrDefault();
-    var expectedToken =  Environment.GetEnvironmentVariable("SECRET");
-
-    if (tokenFromHeader == expectedToken)
-    {
-        await next.Invoke();
-    }
-    else
-    {
-        context.Response.StatusCode = 403;
-        await context.Response.WriteAsync("Forbidden: Invalid frontend token");
-    }
-});
-
     // global error handler
     app.UseMiddleware<ErrorHandlerMiddleware>();
 
